900d813a4c48560b2cb279628556d761247da1be
utamikinanti21 <utamikinanti21@student.upi.edu>
2026-06-02 23:59:26 +0700

    tambahin modul stack

--- stack.cpp
+++ stack.cpp
@@ -1,6 +1,38 @@
-#include<iostream>
-using namespace std ;
+#include "stack.h"
+#include <iostream>
 
-main(){
-    cout << "hello world" << endl ;
+using namespace std;
+
+Stack::Stack()
+{
+    top = -1;
+}
+
+void Stack::push(Position p)
+{
+    if(top < 99)
+    {
+        top++;
+        data[top] = p;
+    }
+}
+
+Position Stack::pop()
+{
+    Position emptyPos = {-1, -1};
+
+    if(isEmpty())
+    {
+        return emptyPos;
+    }
+
+    Position temp = data[top];
+    top--;
+
+    return temp;
+}
+
+bool Stack::isEmpty()
+{
+    return top == -1;
 }
